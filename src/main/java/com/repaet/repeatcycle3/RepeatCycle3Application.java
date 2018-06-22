d9110280da83ceb6ceeeba25b5b674fc1da882c5
masterhwan <mastarhwan@gmail.com>
2018-06-22 21:26:58 +0900

    initialize project

---
+++ src/main/java/com/repaet/repeatcycle3/RepeatCycle3Application.java
@@ -0,0 +1,12 @@
+package com.repaet.repeatcycle3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepeatCycle3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepeatCycle3Application.class, args);
+	}
+}
